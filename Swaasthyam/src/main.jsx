b8a86f5a1694a661a9f5636521f7eac4de285d3e
Krushna968 <krushna968@example.com>
2025-10-02 23:15:18 +0530

    Completely remove DarkModeProvider from app

--- Swaasthyam/src/main.jsx
+++ Swaasthyam/src/main.jsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './LanguageContext.jsx'
-import { DarkModeProvider } from './DarkModeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DarkModeProvider>
-      <LanguageProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </LanguageProvider>
-    </DarkModeProvider>
+    <LanguageProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LanguageProvider>
   </StrictMode>,
 )
