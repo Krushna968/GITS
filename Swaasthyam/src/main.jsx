c92a60318e9095ec5ba3a75a2d0c57a2aca3c36e
Krushna968 <krushna968@example.com>
2025-10-02 23:04:15 +0530

    Fix: Change HashRouter to BrowserRouter for Vercel deployment

--- Swaasthyam/src/main.jsx
+++ Swaasthyam/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './LanguageContext.jsx'
@@ -10,9 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <DarkModeProvider>
       <LanguageProvider>
-        <HashRouter>
+        <BrowserRouter>
           <App />
-        </HashRouter>
+        </BrowserRouter>
       </LanguageProvider>
     </DarkModeProvider>
   </StrictMode>,
