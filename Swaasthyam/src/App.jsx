12a6722466302e613cc84f1ed478d8986626048f
Krushna968 <krushna968@example.com>
2025-10-02 23:09:40 +0530

    Remove dark mode toggle - user preference

--- Swaasthyam/src/App.jsx
+++ Swaasthyam/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home.jsx";
 import LoginPage from "./LoginPage.jsx";
 import OfficerRegistration from "./OfficerRegistration.jsx";
@@ -9,16 +9,10 @@ import OfficialDashboard from "./OfficialDashboard.jsx";
 import MigrantDashboard from "./MigrantDashboard.jsx";
 import DigiLockerMock from "./DigiLockerMock.jsx";
 import Chatbot from "./components/Chatbot.jsx";
-import DarkModeToggle from "./components/DarkModeToggle.jsx";
 
 function App() {
-  const location = useLocation();
-  const isLandingPage = location.pathname === '/' || location.pathname === '/home';
-
   return (
     <>
-      {/* Show Dark Mode Toggle on all pages except landing */}
-      {!isLandingPage && <DarkModeToggle />}
       
       <Routes>
         {/* Public */}
